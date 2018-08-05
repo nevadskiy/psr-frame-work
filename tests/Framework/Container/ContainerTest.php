fd17a5e081fee7718d346c9c667de9d49127192f
Nevadskiy <nevadskiy@gmail.com>
2018-08-05 15:43:05 +0300

    Feature container exception

--- tests/Framework/Container/ContainerTest.php
+++ tests/Framework/Container/ContainerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Framework\Container;
 
 use Framework\Container\Container;
+use Framework\Container\ServiceNotFoundException;
 use PHPUnit\Framework\TestCase;
 
 class ContainerTest extends TestCase
@@ -23,5 +24,15 @@ class ContainerTest extends TestCase
 
         $container->set($name = 'name', $value = new \stdClass());
         $this->assertEquals($value, $container->get($name));
+    }
+
+    /** @test */
+    public function it_expect_not_found()
+    {
+        $container = new Container();
+
+        $this->expectException(ServiceNotFoundException::class);
+
+        $container->get('email');
     }
 }
